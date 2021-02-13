e066b8bec9f6b2f5148a0afb35d36c434459981d
abhaydee <abhaygirish1997@gmail.com>
2021-02-13 19:28:32 +0530

    changes to theme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "98e2938dbfeab9e8d67d";
+/******/ 	var hotCurrentHash = "dde641f3d8bbb04e0776";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -10,9 +10,9 @@ function Header() {
     return (
         <div className={styles["header"]}>
            <h3 className={styles["header__title"]}>Where in the world?</h3>
-           <span> 
-               <img src={theme===true? IconSun:IconMoon} alt="theme-icon" className="header__theme-image" width={30} height={30} onClick={handleTheme}/>
-               <p className={styles["header_themetitle"]}>Dark Mode</p>
+           <span className={styles["header__themesection"]}> 
+               <img src={theme===true? IconSun:IconMoon} alt="theme-icon" className={styles["header__theme-image"]} width={30} height={30} onClick={handleTheme}/>
+               <p className={styles["header__themetitle"]}>Dark Mode</p>
            </span>
         </div>
     )
