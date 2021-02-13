64eb8281dc9d2245b68f86d554ca022830db24a8
abhaydee <abhaygirish1997@gmail.com>
2021-02-13 23:52:11 +0530

    changes to card

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3a254867cf1385f5eb11";
+/******/ 	var hotCurrentHash = "797dea29e51fe72e572d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Components/Countries.jsx
+++ Components/Countries.jsx
@@ -3,15 +3,21 @@ import styles from "../styles/countries.module.scss";
 function Countries({ countries }) {
   return (
     <div className={styles["countries"]}>
-      <div className={styles["countries__card"]}>
-        <img  alt="countries" className={styles["countries__image"]} />
-        <div class="container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect & Engineer</p>
+      {countries?.map((country, index) => (
+        <div key={index} className={styles["countries__card"]}>
+          <img
+            alt="countries"
+            className={styles["countries__image"]}
+            src={country.flag}
+          />
+          <div class="container">
+            <h4>
+              <b>{country.name}</b>
+            </h4>
+            <p>{country.capital}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
